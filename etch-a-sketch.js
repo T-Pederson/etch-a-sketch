c50edefff4292c1961c955a7152f5f74ee22ff29
t <tysonpederson95@gmail.com>
2022-08-19 11:10:49 -0500

    Complete generateGrid, removePixels, and addPixels functions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,42 @@
-// Generate the default 16x16 grid on page load
-let sliderValue = document.querySelector(".slider").value;
-generateGrid(sliderValue);
-
-// Add event listener to slider to update the grid size when slider changes
-document.querySelector(".slider").addEventListener("change", generateGrid);
+// Generate the default 36x36 grid on page load
+let originalSliderValue = document.querySelector(".slider").value;
+const sketchPad = document.querySelector(".sketchPad");
+generateGrid(originalSliderValue);
 
 
 // Create grid of div's based on sliderValue
-function generateGrid(size) {
+function generateGrid(sliderValue) {
     // Delete existing grid
+    removePixels(sketchPad);
 
-    // Create div's for new grid based on slider value
-
+    // Create div's for new grid based on slider value and update sketch pad to fit them
+    addPixels(sketchPad, sliderValue);
 }
 
 
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+// Remove every pixel from sketchPad
+function removePixels(sketchPad) {
+    while (sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
     }
+}
+
+
+// Add pixels to sketchPad according to sliderValue
+function addPixels(sketchPad, sliderValue) {
+    // Find grid size based on slider value
+    let sliderValueSquared = sliderValue * sliderValue;
+
+    // Add pixels to the sketch pad according to slider value
+    for (let i = 0; i < sliderValueSquared; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        sketchPad.appendChild(pixel);
+    }
+
+    // Make sketch pad fit all pixels in a grid
+    sketchPad.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`;
+    sketchPad.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`;
+
+    return;
 }
